eb632fb7f412700512cf1bb644bb6d87c35f7186
garaytj <160015181+garaytj@users.noreply.github.com>
2025-04-01 09:29:53 +0200

    Create user.js

---
+++ js/user.js
@@ -0,0 +1,14 @@
+var user1 = {
+username: '209',
+name: '2°9',
+firstname: 'Classe de',
+password: '209_2024-25',
+home: 'http://lyc-paul-langevin/home/209.html',
+};
+var user2 = {
+username: '',
+name: '',
+firstname: '',
+password: '',
+home: '',
+};
